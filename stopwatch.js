026344679ff44888f464fa4a64dc9af7821d12b4
Awal07 <awalamoo7@gmail.com>
2021-10-17 01:56:35 +0100

    updated stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,10 +1,39 @@
 
 
 function StopWatch () {
-    let startTime, stopTime, duration = 0;
+    let startTime, stopTime, duration, running = 0;
 
 
     this.start = function () {
+        if (running) 
+            throw new Error ('Stopwatch is already running')
 
-    }
+        running = true;
+
+    startTime = new Date ();
+    };
+
+    this.stop = function () {
+        if (!running) 
+            throw new Error ('Stopwatch has not started')
+        
+        running = false;
+
+    stopTime = new Date ();
+
+    const seconds = (stopTime.getTime () - startTime.getTime()) / 1000;
+    duration += seconds;
+    };
+
+    this.reset = function () {
+        startTime = null;
+        stopTime = null;
+        running = false;
+        duration = 0;
+
+    };
+
+    Object.defineProperty (this, 'duration',{
+        get function () { return duration}
+    })
 }
